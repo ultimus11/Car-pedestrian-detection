65b46a37fef9c826497cb8c3071d8dd81c6d6dbe
sahil <sahil11panindre@gmail.com>
2021-03-09 22:03:12 +0530

    Add files via upload

---
+++ code/test1.py
@@ -0,0 +1,15 @@
+from imageai.Detection import ObjectDetection
+
+detector = ObjectDetection()
+
+model_path = "yolo-tiny.h5"
+input_path = "test1.jpg"
+output_path = "newimage.jpg"
+
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(model_path)
+detector.loadModel()
+detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+
+for eachItem in detection:
+    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
