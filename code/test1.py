98eee1a62c5ca46df0fd844cab70914e2dd52b88
sahil <sahil11panindre@gmail.com>
2021-04-08 21:41:39 +0530

    Update test1.py

--- code/test1.py
+++ code/test1.py
@@ -10,6 +10,6 @@ detector.setModelTypeAsTinyYOLOv3()
 detector.setModelPath(model_path)
 detector.loadModel()
 detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
-
+#iterate through the detection
 for eachItem in detection:
     print(eachItem["name"] , " : ", eachItem["percentage_probability"])
